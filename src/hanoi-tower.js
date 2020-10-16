bdef40d7923434acb6a93736259e540df7204e73
Irenebell <56896832+irenebell@users.noreply.github.com>
2020-10-16 23:35:07 +0300

    fix: change

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
+module.exports = function calculateHanoi(disksNumber, turnsSpeed ) {
   let i = 2 ** disksNumber - 1;
   let timeOneStep = turnsSpeed / 3600;
   let seconds = Math.floor(i / timeOneStep);
