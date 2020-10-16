017cfb9d8823c22b0893c14502d4822ecb6fec74
Irenebell <56896832+irenebell@users.noreply.github.com>
2020-10-16 18:01:22 +0300

    fix: add changes

--- src/what-season.js
+++ src/what-season.js
@@ -1,16 +1,18 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function getSeason(/* date */) {
+  
   if (date === undefined) {
     return 'Unable to determine the time of year!';
   }
+
+  if (Object.prototype.toString.call(date) !== '[object Date]') throw new Error('THROWN');
+
   let month = date.getMonth();
   
   if (month == 0 || month == 1 || month == 11) {
    return 'winter';}
   else if (month == 2 || month == 3 || month == 4) {return 'spring';}
    else if  (month == 5 || month == 6 ||  month == 7) {return 'summer';}
-  else if (month == 8  || month == 9 || month == 10) {return 'fall';}
-   
-  else return ('Error');
+  else if (month == 8  || month == 9 || month == 10) {return 'fall';} 
 };
