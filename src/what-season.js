701c722eeaaaa3e6e00d4e9cebaa6518acd3efa0
Irenebell <56896832+irenebell@users.noreply.github.com>
2020-10-16 23:38:02 +0300

    fix: change

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,6 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
+module.exports = function getSeason(date) {
   
   if (date === undefined) {
     return 'Unable to determine the time of year!';
