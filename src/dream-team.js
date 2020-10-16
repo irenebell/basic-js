9a072664ddc79cb649a3a3f74d445891c603e2ee
Irenebell <56896832+irenebell@users.noreply.github.com>
2020-10-16 22:26:06 +0300

    fix: change

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,7 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-    if(Array.isArray(members)) {
+module.exports = function createDreamTeam(members) {  
+  // remove line with error and write your code here
+  if(Array.isArray(members)) {
     let nameTeam = members.map(function (item) {
       if (typeof item == 'string') {
         return item.trim().charAt(0);
