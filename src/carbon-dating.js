8d72b2a3c6e64c3e9fee329dc67a5eb6aa0ae030
Irenebell <56896832+irenebell@users.noreply.github.com>
2020-10-16 23:35:50 +0300

    fix: changes

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,7 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
+module.exports = function dateSample(sampleActivity) {
   if (sampleActivity === NaN) {
     return false;
   }
