a0320abb0834f175ece3354e777119ecb3135fec
Irenebell <56896832+irenebell@users.noreply.github.com>
2020-10-18 00:10:48 +0300

    Update recursive-depth.js

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -2,9 +2,8 @@ const CustomError = require("../extensions/custom-error");
 
 module.exports = class DepthCalculator {
   calculateDepth(arr) {
- if (arr.length === 0) return 1;
+  if (arr.length === 0) return 1;
   if (Array.isArray(arr)) return 1 + Math.max(...arr.map(d => this.calculateDepth(d)));
   else return 0;
-  }
-   
+  }  
 };
