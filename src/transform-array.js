cccc127860b9d5e62dae91be9aa9c68e8f980893
Irenebell <56896832+irenebell@users.noreply.github.com>
2020-10-22 12:04:25 +0300

    fix: solution

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,33 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  if (!Array.isArray(arr)) throw new Error()
+  
+  const result = []
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === NaN) {
+      return arr;
+    } 
+    switch (arr[i]) {
+      case '--discard-prev' :
+        result.pop();
+        break;
+        case "--discard-next" :
+          i+= 2;
+          break;
+          case '--double-prev' :
+            if(result[i - 1] !== undefined) {
+              result.push(arr[i - 1])              
+        }
+        break;
+        case '--double-next' :
+          if (arr[i + 1] !== undefined) {
+            result.push(arr[i + 1]);
+          }
+          break;
+          default :
+          result.push(arr[i]);
+    }
+  }
+        return result;
 };
